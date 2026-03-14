33d655b63d18c67fcd531d1fbd4d36d746b0821f
technik1251 <technik1251@gmail.com>
2026-03-14 09:05:12 +0100

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'styre-cache-v1';
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const resClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, resClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
